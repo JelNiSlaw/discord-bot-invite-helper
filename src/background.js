947bda5c32611c8107184c0728e706fe7df83215
JelNiSlaw <s.jelnicki@gmail.com>
2020-12-16 17:08:59 +0100

    Don't reload the website when URL doesn't change

--- src/background.js
+++ src/background.js
@@ -1,3 +1,10 @@
 chrome.browserAction.onClicked.addListener(({ id, pendingUrl, url }) => {
-	chrome.tabs.update(id, { url: (pendingUrl || url).replace(/(?<=permissions=)\d+/i, '0') });
+	url = pendingUrl || url;
+	let updated = url.replace(/(?<=permissions=)\d+/i, '0');
+
+	if (updated != url) {
+		chrome.tabs.update(id, { url: updated });
+	} else {
+		alert('Permission value not found in the page URL.');
+	}
 });
